92ccd4c3feb03d3e010f3de4befb27bde0f6cf96
Anisha0906 <anishadas880@gmail.com>
2022-08-29 21:03:35 +0530

    [Anisha] Calculating Total Wage for a monthusing While Loop

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main (string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage");
-            Console.WriteLine("Choose Option\n 1.Check Absent or Present\t 2.Calculate Daily Wage\t 3.Part Time Wage\t 4.Emp Wage for month");
+            Console.WriteLine("Choose Option\n 1.Check Absent or Present\t 2.Calculate Daily Wage\t 3.Part Time Wage\t 4.Emp Wage for month\t 5.Total Employee Wages For Month ");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
@@ -21,7 +21,10 @@
                 case 4:
                     EmpWageSwitchCase.WageForMonth();
                     break;
-                   default:
+                case 5:
+                    WagesForMonthWhileLoop.CalculateWage();
+                    break;
+                default:
                     Console.WriteLine("Entered Wrong Choice");
                     break;
             }
