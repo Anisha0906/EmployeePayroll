0b73cc117a7add8589af50aa47fb98fa51b7cfe9
Anisha0906 <anishadas880@gmail.com>
2022-09-07 21:04:37 +0530

    [Anisha] Calculate Employee Wage For Multiple Company

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main (string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage");
-            Console.WriteLine("Choose Option\n 1.Check Absent or Present\t 2.Calculate Daily Wage\t 3.Part Time Wage\t 4.Emp Wage for month\t 5.Total Employee Wages For Month ");
+            Console.WriteLine("Choose Option\n 1.Check Absent or Present\t 2.Calculate Daily Wage\t 3.Part Time Wage\t 4.Emp Wage for month\t 5.Total Employee Wages For Month\t 6.Emp Wage for Multiple Company ");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
@@ -23,6 +23,10 @@
                     break;
                 case 5:
                     WagesForMonthWhileLoop.CalculateWage();
+                    break;
+                case 6:
+                    EmpWageMultipleCo.CalculateMultipleCoWage("DMart", 20, 2, 10);
+                    EmpWageMultipleCo.CalculateMultipleCoWage("BigBazar", 10, 4, 20);
                     break;
                 default:
                     Console.WriteLine("Entered Wrong Choice");
